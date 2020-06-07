9d60b1661da4020cef0d17a53b1324a58034c0bd
yeami <yeamin21@outlook.com>
2020-06-08 04:27:16 +0600

    contact added

--- src/yeamin21/gomail/windows/mail/ControllerInbox.java
+++ src/yeamin21/gomail/windows/mail/ControllerInbox.java
@@ -1,6 +1,7 @@
 package yeamin21.gomail.windows.mail;
 
 
+import javafx.scene.input.MouseEvent;
 import yeamin21.gomail.base.mail.FetchMails;
 import yeamin21.gomail.base.mail.Mails;
 import yeamin21.gomail.windows.signings.ControllerLogin;
@@ -17,30 +18,49 @@ import java.util.ResourceBundle;
 
 public class ControllerInbox implements Initializable {
 
-
+    @FXML
+    TextArea mailBody;
     @FXML
     TableColumn cSender,cDate,cSubject,cBody,cButton;
     @FXML
     TableView aTable;
+    @FXML
     ObservableList<Mails> data= FXCollections.observableArrayList();;
    @Override
     public void initialize(URL location, ResourceBundle resources) {
        FetchMails mail=new FetchMails(ControllerLogin.userEmail);
-        mail.Read();
+       mail.Read();
        for(Mails aMail:mail.mails)
        {
            data.add(aMail);
        }
 
-      cSender.setCellValueFactory(new PropertyValueFactory<>("sender"));
-     cDate.setCellValueFactory(new PropertyValueFactory<>("date"));
-     cSubject.setCellValueFactory(new PropertyValueFactory<>("subject"));
-    cBody.setCellValueFactory(new PropertyValueFactory<>("body"));
+       cSender.setCellValueFactory(new PropertyValueFactory<>("sender"));
+       cDate.setCellValueFactory(new PropertyValueFactory<>("date"));
+       cSubject.setCellValueFactory(new PropertyValueFactory<>("subject"));
+       cBody.setCellValueFactory(new PropertyValueFactory<>("body"));
 
        aTable.setItems(data);
+
+
+
+
+
+
+
+    }
+    public void onTableClick() {
+        if (aTable.getSelectionModel().getSelectedItem() != null) {
+            Mails aMail = (Mails) aTable.getSelectionModel().getSelectedItem();
+            mailBody.setText("From: "+aMail.getSender()+"\n");
+            mailBody.appendText("Subject: "+aMail.getSubject()+"\n");
+            mailBody.appendText("\n"+aMail.getBody()+"\n");
+
+        }
     }
 
-    private void mew() {
-        System.out.println("C");
-    }
+
+
+
+
 }
