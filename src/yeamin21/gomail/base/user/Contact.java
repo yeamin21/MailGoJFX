7867a11ab81c1b9574047165a8deb8e263314fa8
yeamin21 <yeamin21@outlook.com>
2020-06-16 01:02:42 +0600

    Code Updated

--- src/yeamin21/gomail/base/user/Contact.java
+++ src/yeamin21/gomail/base/user/Contact.java
@@ -16,52 +16,6 @@ import java.sql.SQLException;
  *
  * @author yeami
  */
-public class Contact implements DatabaseOperations {
-    private String user,contact;
+public class Contact extends Users {
 
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
-
-    @Override
-    public void Create() {
-        String sqlAddToContact="INSERT INTO CONTACTS(user_email,contact_email) values(?,?)";
-        Connection con= ConnectDB.connect();
-        try {
-          PreparedStatement stm= con.prepareStatement(sqlAddToContact);
-          stm.setString(1,this.getUser());
-          stm.setString(2,this.getContact());
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-    }
-
-    @Override
-    public void Read() {
-
-
-    }
-
-    @Override
-    public void Update() {
-
-    }
-
-    @Override
-    public void Delete() {
-
-    }
 }
--- src/yeamin21/gomail/base/user/UserContacts.java
+++ src/yeamin21/gomail/base/user/UserContacts.java
@@ -3,18 +3,26 @@ package yeamin21.gomail.base.user;
 import yeamin21.gomail.base.mail.DatabaseOperations;
 import yeamin21.gomail.base.mail.Mails;
 import yeamin21.gomail.windows.ConnectDB;
+import yeamin21.gomail.windows.mail.ControllerComposePanel;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class UserContacts implements DatabaseOperations {
     private Mails mail;
+    private Contact contact;
     Connection con= ConnectDB.connect();
     public String loggedInUser;
+    ArrayList<Contact>contacts=new ArrayList<>();
     public UserContacts(Mails mail)
     {
        this.mail=mail;
+    }
+    public UserContacts(Contact contact){
+        this.contact=contact;
     }
 
     @Override
@@ -33,16 +41,41 @@ public class UserContacts implements DatabaseOperations {
 
     @Override
     public void Read() {
+        String SQL_GETCONTACTS="SELECT contact_email,first_name,last_name "+
+                "from  contacts right join `user` " +
+                "on contacts.contact_email=`user`.email where user_email='"+this.loggedInUser+"'";
+
+        try{
+            PreparedStatement stm = con.prepareStatement(SQL_GETCONTACTS);
+            ResultSet rs=stm.executeQuery();
+            while (rs.next())
+            {
+                Contact contact=new Contact();
+                contact.setFirstName("first_name");
+                contact.setEmail("contact_email");
+                contact.setLastName("last_name");
+                contacts.add(contact);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
     }
 
     @Override
     public void Update() {
-
     }
 
     @Override
     public void Delete() {
+        String SQL_DELETECONTACT="DELETE FROM CONTACTS WHERE `user_email`='"+this.loggedInUser+
+                "' and `contact_email`='"+this.contact.getEmail()+"'";
 
+        try{
+            PreparedStatement stm = con.prepareStatement(SQL_DELETECONTACT);
+            stm.execute();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
