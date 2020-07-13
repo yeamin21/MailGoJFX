f74b4b1397592fa65569af974068f8e745c57a4c
yeamin21 <yeamin21@Ourlook.com>
2020-07-14 04:00:38 +0600

    vbox

--- src/yeamin21/gomail/windows/Main.java
+++ src/yeamin21/gomail/windows/Main.java
@@ -19,6 +19,5 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
-
     }
 }
--- src/yeamin21/gomail/windows/mail/ControllerMailbox.java
+++ src/yeamin21/gomail/windows/mail/ControllerMailbox.java
@@ -1,5 +1,7 @@
 package yeamin21.gomail.windows.mail;
 
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import yeamin21.gomail.windows.SwitchPane;
 import yeamin21.gomail.windows.signings.ControllerLogin;
 import javafx.fxml.FXML;
@@ -14,11 +16,26 @@ public class ControllerMailbox {
     Button buttonComposeMail,buttonInbox;
     @FXML
     Label labelUser;
+    @FXML
+    VBox vbox;
+
 
 
     @FXML
     public void initialize(){
         labelUser.setText(ControllerLogin.userEmail);
+        new SwitchPane("mail\\Inbox.fxml",panelMailboxBody).doSwitch();
+
+
+        for(int i=0;i<5;i++)
+        {
+
+            Button l=new Button("ABBIIIFS");
+            AnchorPane anchorPane=new AnchorPane();
+            anchorPane.getChildren().addAll(l);
+            vbox.getChildren().add(anchorPane);
+            System.out.println(vbox.getChildren());
+        }
     }
 
     @FXML
